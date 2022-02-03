50629edcb2eabeea63abf9b384d5ea0d2be49487
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2022-02-03 08:13:33 -0800

    fix(query): ensure input variable names with special substrings do not affect parsing of table refs (#58)

--- src/logic/sqlToTypeDefinitions/query/extractTableReferencesFromQuerySql/extractTableReferenceSqlSetFromQuerySql.ts
+++ src/logic/sqlToTypeDefinitions/query/extractTableReferencesFromQuerySql/extractTableReferenceSqlSetFromQuerySql.ts
@@ -6,10 +6,10 @@ import { TABLE_REFERENCE_TYPE } from './constants';
 export const extractTableReferenceSqlSetFromQuerySql = ({ sql }: { sql: string }) => {
   // 1. grab the content of everything between FROM and WHERE
   const everythingAfterFromInclusive = (() => {
-    const partsSplitOnSelect = sql.split(/[\n\r\s]+from[\n\r\s]+/gi);
-    if (partsSplitOnSelect.length === 1) throw new Error('no "from" keyword found'); // fail fast; allow this being caught above
-    if (partsSplitOnSelect.length > 2) throw new Error('more than one "from" keyword found; not yet supported'); // TODO: https://github.com/uladkasach/sql-code-generator/issues/2
-    return `FROM ${partsSplitOnSelect[1]}`; // inclusive, since we include "from"
+    const partsSplitOnFrom = sql.split(/[\n\r\s]+from[\n\r\s]+/gi);
+    if (partsSplitOnFrom.length === 1) throw new Error('no "from" keyword found'); // fail fast; allow this being caught above
+    if (partsSplitOnFrom.length > 2) throw new Error('more than one "from" keyword found; not yet supported'); // TODO: https://github.com/uladkasach/sql-code-generator/issues/2
+    return `FROM ${partsSplitOnFrom[1]}`; // inclusive, since we include "from"
   })();
   const everythingBetweenFromAndWhere = (() => {
     const partsSplitOnFromAfterSelect = everythingAfterFromInclusive.split(/(?:WHERE|where)/g);
@@ -21,7 +21,9 @@ export const extractTableReferenceSqlSetFromQuerySql = ({ sql }: { sql: string }
 
   // 3. split token on each table reference type "FROM", "JOIN", "INNER JOIN", and "LEFT JOIN"
   const tableReferenceDefinitionSplitRegex = new RegExp(
-    `(${TABLE_REFERENCE_TYPE.join('|')}|${TABLE_REFERENCE_TYPE.map((str) => str.toLowerCase()).join('|')})`,
+    `(?:[\\n\\r\\s]+)(${TABLE_REFERENCE_TYPE.join('|')}|${TABLE_REFERENCE_TYPE.map((str) => str.toLowerCase()).join(
+      '|',
+    )})(?:[\\n\\r\\s]+)`,
     'g',
   );
   const partsSplitOnJoinType = ` ${everythingBetweenFromAndWhere} `.split(tableReferenceDefinitionSplitRegex);
@@ -34,7 +36,7 @@ export const extractTableReferenceSqlSetFromQuerySql = ({ sql }: { sql: string }
       if (partialTableReferenceSql !== null) throw new Error('unexpected'); // fail fast, this should never occur
       partialTableReferenceSql = part; // initialize the partial table reference
     } else {
-      tableReferenceSqlSet.push((partialTableReferenceSql + part).trim().replace(';', ''));
+      tableReferenceSqlSet.push(`${partialTableReferenceSql} ${part}`.trim().replace(';', ''));
       partialTableReferenceSql = null;
     }
   });
